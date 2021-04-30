1355269fd1e1f5b5f369b490c9fc6b571c9e8c00
David Fernandez Cueto <david.fernandez.cueto@lambdamotive.com>
2021-04-30 14:52:00 +0200

    prueba

---
+++ gamerate/src/components/CoverGames.vue
@@ -0,0 +1,22 @@
+<template>
+  <card-game :title="title">
+  </card-game>
+</template>
+
+<script>
+import CardGame from './CardGame.vue'
+export default {
+  components: { CardGame },
+
+  data() {
+      return {
+          title: "title"
+      }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
