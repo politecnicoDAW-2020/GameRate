2a4691c9f2e7ab944676585d32299ac484a66801
David Fernandez Cueto <david.fernandez.cueto@lambdamotive.com>
2021-05-04 16:27:31 +0200

    auth logic

---
+++ gameratenfront/src/logic/auth.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const ENDPOINT_PATH = "";
+
+export default {
+    setUserLogged(userLogged) {
+        Cookies.set("userLogged", userLogged);
+    },
+    getUserLogged() {
+        return Cookies.get("userLogged");
+    },
+    deleteUserLogged() {
+        Cookies.remove('userLogged');
+    },
+    register(email, password) {
+        const user = {email, password};
+        return axios.post(ENDPOINT_PATH + "register", user);
+    },
+    login(email, password) {
+        const user = { email, password };
+        return axios.post(ENDPOINT_PATH + "login", user);
+    }
+};
