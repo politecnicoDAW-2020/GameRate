da49f614855a658e2844161efabf40a35ec93df3
David Fernandez Cueto <david.fernandez.cueto@lambdamotive.com>
2021-05-04 13:26:23 +0200

    Rating component

---
+++ gameratenfront/src/components/Rating.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-rating
+  color="warning"
+  half-increments
+  hover
+  length="5"
+  value="3"
+  size="24"
+></v-rating>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
